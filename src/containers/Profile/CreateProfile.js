ff992dfb6de783ad1ffd2f89b5b40746cf2e6a9c
Duncan Morrison <duncanmorrison2001@yahoo.com>
2020-12-17 12:55:35 +0000

    formatted profile, added some garbage welcome text

--- src/containers/Profile/CreateProfile.js
+++ src/containers/Profile/CreateProfile.js
@@ -1,10 +1,26 @@
 import { Component } from 'react';
+import classes from './Profile.module.css';
 import axios from '../../axios-instance';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class CreateProfile extends Component {
   state = {
-    
+    languages: null
+  }
+
+  componentDidMount() {
+    this.getLanguages();
+  }
+
+  getLanguages = () => {
+    axios.get('/languages')
+      .then(response => {
+        const languageArray = response.data;
+        this.setState({
+          languages: languageArray
+        })
+      })
   }
 
   handleInputChange = (event) => {
@@ -33,6 +49,8 @@ class CreateProfile extends Component {
         delete body[language];
       }
     }
+    //remove languages array
+    delete body["languages"]
     console.log(body)
     axios.post('/profile/' + this.props.user.id, body)
       .then(response => {
@@ -45,31 +63,37 @@ class CreateProfile extends Component {
   }
 
   render() {
-    //add more languages here to add to the form
-    const languages = ["java", "python", "ruby", "javascript", "C#", "C", "PHP", "swift", "go"];
+    let languageInputs = <Spinner />;
 
-    //make each language into a select input
-    let languageInputs = languages.map(language => {
-      return <div>
-        <label htmlFor={language}>{language}:</label>
-        <select name={language} id={language} onChange={this.handleInputChange}>
-          <option value="0">Not Interested</option>
-          <option value="1">Beginner</option>
-          <option value="2">Intermediate</option>
-          <option value="3">Advanced</option>
-        </select>
-      </div>
-    })
+    if(this.state.languages) {
+      //make each language into a select input
+      languageInputs = this.state.languages.map(language => {
+        return <div key={language.id}>
+          <label className={classes.LanguageLabel} htmlFor={language.name}>{language.name}:</label>
+          <select name={language.name} id={language.name} onChange={this.handleInputChange}>
+            <option value="0">Not Interested</option>
+            <option value="1">Beginner</option>
+            <option value="2">Intermediate</option>
+            <option value="3">Advanced</option>
+          </select>
+        </div>
+      })
+    }
+    
 
     return(
       <div>
+        <h3>Please complete your user profile to be eligible for pairing!</h3>
         <form onSubmit={this.submitHandler}>
+        <p>Github is a great place to store your code, so others can see what kind of projects you've been working on.</p>
+        <p>Enter your link below, or open a free account here: <a href="https://github.com/">Github</a></p>
         <input 
         type="text" 
         placeholder="Your github link" 
         name="github" 
         onChange={(event) => this.handleInputChange(event)}
         />
+        <p>Let us know your interest and experience levels:</p>
         {languageInputs}
         <Button btnType="Success">Submit</Button>
         </form>
